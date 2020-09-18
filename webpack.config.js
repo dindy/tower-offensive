d08d28426400fb49a5720999416d1a88211c718e
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2020-09-18 16:03:18 +0200

    Allow local access to dev server

--- webpack.config.js
+++ webpack.config.js
@@ -3,34 +3,39 @@ const webpack = require("webpack")
 const env = process.env.NODE_ENV
 
 let config = {
-  mode: env || 'development',
-  module: {
-    rules: [{
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-          options: { presets: ["@babel/preset-env"] }
-        }
-    }]
-  },
-  output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "bundle.js",
-    globalObject: 'this'
-  },  
-  node: {
-    fs: 'empty',
-  },
-  plugins: []  
+    mode: env || 'development',
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            use: {
+                loader: "babel-loader",
+                options: { presets: ["@babel/preset-env"] }
+            }
+        }]
+    },
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "bundle.js",
+        globalObject: 'this'
+    },  
+    node: {
+        fs: 'empty',
+    },
+    plugins: [],
+    devServer: {
+        host: '0.0.0.0',//your ip address
+        port: 8080,
+        disableHostCheck: true
+    }  
 }
 
 module.exports = (env, argv) => {
-  if (env === 'development') {
-    config.entry = ['webpack/hot/dev-server', "./src/index.js"] 
-    config.devtool = 'inline-source-map'    
-  } else {
-    config.entry = ["./src/index.js"] 
-  }
-  return config
+    if (env === 'development') {
+        config.entry = ['webpack/hot/dev-server', "./src/index.js"] 
+        config.devtool = 'inline-source-map'    
+    } else {
+        config.entry = ["./src/index.js"] 
+    }
+    return config
 }
